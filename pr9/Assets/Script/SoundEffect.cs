a1e5f5d42b029cbad4fe66f336a3cb7a0560feb7
Averchenkov <nicroniys@gmail.com>
2020-04-23 00:56:02 +0300

    add pr9

---
+++ pr9/Assets/Script/SoundEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioSource SoundEff;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        SoundEff.Play();
+    }
+}
